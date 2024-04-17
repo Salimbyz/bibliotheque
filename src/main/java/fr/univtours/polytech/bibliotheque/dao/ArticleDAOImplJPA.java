edd8e02a3f31fb9ddfe9174acb8430180939c7e3
JimmmyBoy <polo.p0741@gmail.com>
2024-04-17 13:53:20 +0200

    business

--- src/main/java/fr/univtours/polytech/bibliotheque/dao/ArticleDAOImplJPA.java
+++ src/main/java/fr/univtours/polytech/bibliotheque/dao/ArticleDAOImplJPA.java
@@ -1,14 +1,5 @@
 package fr.univtours.polytech.bibliotheque.dao;
-<<<<<<< HEAD
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-=======
 
->>>>>>> 06bad009e12253de3b87ab91f2b8849a4af0aa7e
 import java.util.List;
 
 import fr.univtours.polytech.bibliotheque.model.ArticleBean;
@@ -38,58 +29,9 @@ public class ArticleDAOImplJPA implements ArticleDAO {
        // TODO Auto-generated method stub
    }
 
-<<<<<<< HEAD
-        try {
-            connection = ConnectionDB.createConnection();
-            final String sqlInsert = "INSERT INTO ARTICLE (ID, NAME, PRICE, NBRESTANT) values (?, ?, ?, ?);";
-            st = connection.prepareStatement(sqlInsert);
-            st.setInt(1, article.getId());
-            st.setString(2, article.getName());
-            st.setFloat(3,article.getPrice());
-            st.setInt(4,article.getNbRestant());
-            st.executeUpdate();
-        } catch (Exception e) {
-            // S'il y a eu un problème, on le fait remonter.
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                // Dans tous les cas, on ferme tout ce qui doit l'être.
-                st.close();
-                connection.close();
-            } catch (Exception e) {
-                // S'il y a eu un problème, on le fait remonter.
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    @Override
-    public void updateArticle(ArticleBean article){
-
-    }
-
-    private final List<ArticleBean> mapResultSetToList(final ResultSet rs) throws SQLException {
-        List<ArticleBean> articleList = new ArrayList<ArticleBean>();
-        while (rs.next()) {
-            // Pour chaque ligne de la table,
-            // on instancie un nouveau NoteBean.
-            final ArticleBean articleBean = new ArticleBean();
-            articleBean.setId(rs.getInt("ID")); // Il faut indiquer le nom du champ en BDD, ici, 'ID_NOTE'.
-            articleBean.setName(rs.getString("NAME"));
-            articleBean.setPrice(rs.getFloat("PRIX"));
-            articleBean.setNbRestant(rs.getInt("NBRESTANT"));
-            // On ajoute ce bean à la liste des résultats.
-            articleList.add(articleBean);
-        }
-        return articleList;
-    }
-}
-
-=======
    @Override
    public void insertArticle(ArticleBean article) {
        // Insertion d'un enregistrement en BDD.
        em.persist(article);
    }
 }
->>>>>>> 06bad009e12253de3b87ab91f2b8849a4af0aa7e
--- src/main/java/fr/univtours/polytech/bibliotheque/dao/UserDAOImplJPA.java
+++ src/main/java/fr/univtours/polytech/bibliotheque/dao/UserDAOImplJPA.java
@@ -1,17 +1,7 @@
 package fr.univtours.polytech.bibliotheque.dao;
-<<<<<<< HEAD
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-=======
 
 import java.util.List;
 
->>>>>>> 06bad009e12253de3b87ab91f2b8849a4af0aa7e
 import fr.univtours.polytech.bibliotheque.model.UserBean;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
@@ -21,19 +11,9 @@ import jakarta.persistence.Query;
 @Stateless
 public class UserDAOImplJPA implements UserDAO {
 
-<<<<<<< HEAD
-    @Override
-    public List<UserBean> getUserList() {
-
-        Statement st = null;
-        ResultSet rs = null;
-        Connection connection = null;
-        List<UserBean> userList = null;
-=======
    // L'objet EntityManager qui va permettre d'effectuer les requêtes en BDD.
    @PersistenceContext(unitName = "bibliotheque")
    private EntityManager em;
->>>>>>> 06bad009e12253de3b87ab91f2b8849a4af0aa7e
 
    @SuppressWarnings("unchecked") 
    @Override
@@ -61,53 +41,7 @@ public class UserDAOImplJPA implements UserDAO {
         try {
             return (String) query.getSingleResult();
         } catch (Exception e) {
-<<<<<<< HEAD
-            // S'il y a eu un problème, on le fait remonter.
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                // Dans tous les cas, on ferme tout ce qui doit l'être.
-                st.close();
-                connection.close();
-            } catch (Exception e) {
-                // S'il y a eu un problème, on le fait remonter.
-                throw new RuntimeException(e);
-            }
-        }
-        return userList;
-    }
-
-    @Override
-    public void insertUser(UserBean user) {
-
-        // On utilisera ici des PreparedStatement plutôt que des Statement pour des
-        // raisons de sécurité.
-        PreparedStatement st = null;
-        Connection connection = null;
-
-        try {
-            connection = ConnectionDB.createConnection();
-            final String sqlInsert = "INSERT INTO USER (login, name, password) values (?, ?, ?);";
-            st = connection.prepareStatement(sqlInsert);
-            st.setString(1, user.getLogin());
-            st.setString(2, user.getName());
-            st.setString(3, user.getPassword());
-            st.executeUpdate();
-        } catch (Exception e) {
-            // S'il y a eu un problème, on le fait remonter.
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                // Dans tous les cas, on ferme tout ce qui doit l'être.
-                st.close();
-                connection.close();
-            } catch (Exception e) {
-                // S'il y a eu un problème, on le fait remonter.
-                throw new RuntimeException(e);
-            }
-=======
             return null; // Gérer l'exception ou renvoyer null si aucun utilisateur n'est trouvé
->>>>>>> 06bad009e12253de3b87ab91f2b8849a4af0aa7e
         }
     }
 }
