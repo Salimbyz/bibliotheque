57d5bd4caab95b885012be031822136c9b12b29d
Salim <benameur.salim94@gmail.com>
2024-04-17 15:50:49 +0200

    ok ok

--- src/main/java/fr/univtours/polytech/bibliotheque/business/ArticleBusinessImpl.java
+++ src/main/java/fr/univtours/polytech/bibliotheque/business/ArticleBusinessImpl.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import fr.univtours.polytech.bibliotheque.dao.ArticleDAO;
 import fr.univtours.polytech.bibliotheque.model.ArticleBean;
+import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 
+@Stateless
 public class ArticleBusinessImpl {
     
     @Inject
